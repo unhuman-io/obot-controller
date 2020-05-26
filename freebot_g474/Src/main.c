baeeee7e6a0ce12d3be9e8bbe6e0b7b3912b0cf4
Lee Magnusson <lee@unhuman.io>
2020-05-26 11:30:21 -0700

    vbus no preserve

--- freebot_g474/Src/main.c
+++ freebot_g474/Src/main.c
@@ -312,7 +312,7 @@ static void MX_ADC1_Init(void)
   hadc1.Init.ExternalTrigConv = ADC_EXTERNALTRIG_HRTIM_TRG1;
   hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_RISING;
   hadc1.Init.DMAContinuousRequests = DISABLE;
-  hadc1.Init.Overrun = ADC_OVR_DATA_PRESERVED;
+  hadc1.Init.Overrun = ADC_OVR_DATA_OVERWRITTEN;
   hadc1.Init.OversamplingMode = DISABLE;
   if (HAL_ADC_Init(&hadc1) != HAL_OK)
   {

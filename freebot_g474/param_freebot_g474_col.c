d0a83c56f2ba89817ab112d6fb828a5f64484e5c
Lee Magnusson <lee@unhuman.io>
2020-07-24 12:27:57 -0700

    quiet warning

--- freebot_g474/param_freebot_g474_col.c
+++ freebot_g474/param_freebot_g474_col.c
@@ -67,4 +67,7 @@ const volatile Param __attribute__ ((section ("flash_param"))) param_store = {
 #endif
 };
 const volatile char * const name = param_store.name;
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdiscarded-qualifiers"
 const Param * const param = &param_store; // todo figure out a way to not inline without warning
+#pragma GCC diagnostic pop

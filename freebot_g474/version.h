3bcf705534930909a8b036bb756c07873dcdae19
Lee Magnusson <lee@unhuman.io>
2020-11-05 08:47:40 -0800

    remove version

--- freebot_g474/version.h
+++ freebot_g474/version.h
@@ -1,7 +1,6 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION "0.1.0"
 #define BUILD_DATETIME __DATE__ " " __TIME__
 #define GIT_HASH GIT_VERSION
 #define PRODUCT_STRING "Freebot G474 Motor"

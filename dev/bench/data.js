4d4e16bf1f6926db260b489d4973ecba805c803f
github-action-benchmark <github@users.noreply.github.com>
2021-04-01 06:41:08 -0700

    add Benchmark (go) benchmark result for d13e3a2057f99906e2aa4e4eb409107f5bbd2b91

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1617284053194,
+  "lastUpdate": 1617284468429,
   "repoUrl": "https://github.com/unhuman-io/freebot-controller",
   "entries": {
     "Benchmark": [
@@ -138,6 +138,52 @@ window.BENCHMARK_DATA = {
             "value": 16999.6,
             "unit": "cycles",
             "extra": "17028 times"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "lee@unhuman.io",
+            "name": "Lee Magnusson"
+          },
+          "committer": {
+            "email": "lee@unhuman.io",
+            "name": "Lee Magnusson"
+          },
+          "distinct": false,
+          "id": "d13e3a2057f99906e2aa4e4eb409107f5bbd2b91",
+          "message": "benchmark\n\n\ntest9\n\n\ntest10\n\n\ntest11\n\n\ntest12\n\n\ntest13\n\n\ntest 14\n\n\ntest 15\n\n\ntest 16\n\n\nbtest1\n\n\nbtest2\n\n\nbtest3\n\n\nbtest4\n\n\nbtest5\n\n\nbtest6\n\n\nbtest7\n\n\nbtest8\n\n\nbtest9\n\n\nbtest13\n\n\nbtest14\n\n\nbtest15\n\n\nbtest16\n\n\nbtest 17\n\n\nbtest 18\n\n\nbtest 19\n\n\nbtest 20\n\n\nbtest 21\n\n\nbtest 22\n\n\nbtest 23",
+          "timestamp": "2021-04-01T06:36:39-07:00",
+          "tree_id": "1fc92154bc6da37b2bb6bc479c830050e2be5e25",
+          "url": "https://github.com/unhuman-io/freebot-controller/commit/d13e3a2057f99906e2aa4e4eb409107f5bbd2b91"
+        },
+        "date": 1617284466618,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "Benchmarkmean_fast_loop_cycles",
+            "value": 577,
+            "unit": "cycles",
+            "extra": "577 times"
+          },
+          {
+            "name": "Benchmarkmean_fast_loop_period",
+            "value": 3396.6,
+            "unit": "cycles",
+            "extra": "3406 times"
+          },
+          {
+            "name": "Benchmarkmean_main_loop_cycles",
+            "value": 2614.33,
+            "unit": "cycles",
+            "extra": "5968 times"
+          },
+          {
+            "name": "Benchmarkmean_main_loop_period",
+            "value": 16999.4,
+            "unit": "cycles",
+            "extra": "17048 times"
           }
         ]
       }

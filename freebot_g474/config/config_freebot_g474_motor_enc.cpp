20529d4103cca67aa85c6b806323492f87fe181e
Lee Magnusson <lee@unhuman.io>
2021-04-12 12:14:10 -0700

    add output encoder to motor_enc

--- freebot_g474/config/config_freebot_g474_motor_enc.cpp
+++ freebot_g474/config/config_freebot_g474_motor_enc.cpp
@@ -3,10 +3,11 @@
 #include "../../motorlib/qep_encoder.h"
 #include "../../motorlib/torque_sensor.h"
 #include "../../motorlib/gpio.h"
+#include "../../motorlib/ma732_encoder.h"
 
 using TorqueSensor = TorqueSensorBase;
 using MotorEncoder = QEPEncoder;
-using OutputEncoder = EncoderBase;
+using OutputEncoder = MA732Encoder;
 
 extern "C" void SystemClock_Config();
 void pin_config_freebot_g474_motor_r0();
@@ -15,6 +16,9 @@ struct InitCode {
     InitCode() {
       SystemClock_Config();
       pin_config_freebot_g474_motor_r0();
+      SPI3->CR2 = (15 << SPI_CR2_DS_Pos);   // 16 bit
+      // ORDER DEPENDANCE SPE set last
+      SPI3->CR1 = SPI_CR1_MSTR | (3 << SPI_CR1_BR_Pos) | SPI_CR1_SSI | SPI_CR1_SSM | SPI_CR1_SPE;    // baud = clock/16
     }
 };
 
@@ -23,12 +27,23 @@ namespace config {
     const uint32_t pwm_frequency = 50000;
     InitCode init_code;
 
-    QEPEncoder motor_encoder = {*TIM2};
+    QEPEncoder motor_encoder(*TIM2);
     TorqueSensor torque_sensor;
-    OutputEncoder output_encoder;
+    GPIO motor_encoder_cs(*GPIOD, 2, GPIO::OUTPUT);
+    MA732Encoder output_encoder(*SPI3, motor_encoder_cs);
 };
 
 #include "config_freebot_g474_motor.cpp"
 
-void config_init() {}
+void config_init() {
+    System::api.add_api_variable("jbct", new APICallbackUint32([](){ return config::output_encoder.get_bct(); },
+                    [](uint32_t u){ config::output_encoder.set_bct(u); }));
+    System::api.add_api_variable("jet", new APICallbackUint32([](){ return config::output_encoder.get_et(); },
+                    [](uint32_t u){ config::output_encoder.set_et(u); }));
+    System::api.add_api_variable("jmgt", new APICallbackUint32([](){ return config::output_encoder.get_magnetic_field_strength(); },
+                    [](uint32_t u){ config::output_encoder.set_mgt(u); }));
+    System::api.add_api_variable("jfilt", new APICallbackUint32([](){ return config::output_encoder.get_filt(); }, 
+                [](uint32_t u){ config::output_encoder.set_filt(u); }));
+}
+
 void config_maintenance() {}

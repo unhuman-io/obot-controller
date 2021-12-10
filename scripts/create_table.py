71d9543b027f029c9daddd9f4c601f09fc02e963
Lee Magnusson <lee@unhuman.io>
2021-12-10 15:07:35 -0800

    high speed calibration

--- scripts/create_table.py
+++ scripts/create_table.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+
+# requirements scipy matplotlib numpy
+
+import numpy as np
 from numpy import *
 from scipy.fftpack import *
 from matplotlib.pyplot import *
@@ -6,6 +10,26 @@ from matplotlib.pyplot import *
 from circular_filt import *
 import argparse
 import os
+
+# from https://stackoverflow.com/questions/27138751/preventing-plot-joining-when-values-wrap-in-matplotlib-plots
+def unlink_wrap(dat, lims=[-np.pi, np.pi], thresh = 0.5):
+    """
+    Iterate over contiguous regions of `dat` (i.e. where it does not
+    jump from near one limit to the other).
+
+    This function returns an iterator object that yields slice
+    objects, which index the contiguous portions of `dat`.
+
+    This function implicitly assumes that all points in `dat` fall
+    within `lims`.
+
+    """
+    jump = np.nonzero(np.abs(np.diff(dat)) > ((lims[1] - lims[0]) * thresh))[0]
+    lasti = 0
+    for ind in jump:
+        yield slice(lasti, ind + 1)
+        lasti = ind + 1
+    yield slice(lasti, len(dat))
 
 
 class Table:
@@ -19,28 +43,52 @@ class Table:
         parser.add_argument('-n', '--gear-ratio',dest="gear_ratio",type=float,default=1.)
         parser.add_argument('-e', '--encoder',dest="encoder_table", action="store_true", default=False)
         parser.add_argument('-j', '--joint',dest="joint_table", action="store_true", default=False)
+        parser.add_argument('--estimate-velocity', dest="estimate_velocity", action="store_true", default=False)
+        parser.add_argument('-f', '--filter',dest="filter", type=float,default=None)
         self.args = parser.parse_args(args_in)
 
         self.data = genfromtxt(self.args.infile, delimiter=',', names=True)
         if (self.args.encoder_table):
-            self.table = self.parse_encoder_table(nbins=self.args.table_size,index_pos=self.args.index_pos,cpr=self.args.cpr)
+            self.table = self.parse_encoder_table(nbins=self.args.table_size,index_pos=self.args.index_pos,cpr=self.args.cpr, estimate_velocity=self.args.estimate_velocity, filter=self.args.filter)
         elif (self.args.joint_table):
             self.table = self.parse_joint_table(nbins=self.args.table_size,index_pos=self.args.index_pos,n=self.args.gear_ratio)
         else:
             self.table = self.parse_cogging_table(nbins=self.args.table_size,index_pos=self.args.index_pos,cpr=self.args.cpr)
         self.save(self.args.outfile)
 
-    def parse_encoder_table(self, nbins=32, index_pos=0, cpr=8192):
+    def velocity_average(self, t, data):
+        velocity = np.diff(data)/np.diff(t)
+        # simple
+        velocity_avg = mean(velocity)
+        pos_avg = t*velocity_avg
+        # N = 10000
+        # velocity_avg = np.convolve(velocity, np.ones(N)/N, mode='same')
+        # #velocity_avg = np.insert(velocity_avg,0,velocity_avg[0])
+        # pos_avg = np.diff(t)*velocity_avg
+        # pos_avg = np.insert(pos_avg,0,0)
+
+        # figure()
+        # plot(velocity_avg)
+        # show()
+        return pos_avg
+
+    def parse_encoder_table(self, nbins=32, index_pos=0, cpr=8192, estimate_velocity=False, filter=2):
+        if not filter:
+            filter = 2
         e = self.data["motor_encoder0"]-float(index_pos)
-        vpos = self.data["motor_position0"]
+        vpos = None
+        if estimate_velocity:
+            vpos = self.velocity_average(self.data["t_seconds0"], e*2*pi/float(cpr))
+        else:
+            vpos = self.data["motor_position0"]
         vpos = unwrap(vpos, 2*pi)
-        y = vpos + e*2*pi/float(cpr)
+        y = vpos - e*2*pi/float(cpr)
         x = e*2*pi/float(cpr)     
         # i = where(abs(vpos) > .9*max(vpos))
         # x = delete(x, i) 
         # y = delete(y, i)
 
-        return self.create_table(x, -y, nbins, 2)
+        return self.create_table(x, -y, nbins, filter)
 
     def parse_cogging_table(self, nbins=32, index_pos=0, cpr=8192):
         e = self.data["motor_encoder0"]-float(index_pos)
@@ -81,7 +129,10 @@ class Table:
         # ylabel('raw y')
 
         figure()
-        plot(mod(x,2*pi), y-mean(y),'.')
+        xmod = mod(x,2*pi)
+        ymod = y-mean(y)
+        for slc in unlink_wrap(xmod, [0, 2*pi]):
+            plot(xmod[slc], ymod[slc], 'b-')
         plot(xfilt,yfilt)
         plot(xfilt, pchip[:,0], '*')
         plot(xcalc, ycalc,'.')
@@ -89,7 +140,7 @@ class Table:
         xlabel('xfilt % 2*pi')
         ylabel('yfilt')
         title('table value y vs input x')
-        legend(['raw','filtered','cubic points','cubic spline'])
+        #legend(['raw','filtered','cubic points','cubic spline'])
         grid()
         show()
         return pchip

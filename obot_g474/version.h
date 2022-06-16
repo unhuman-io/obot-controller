4bc4cb148e4edd06a30bc866b2de1113be07c5b4
Lee Magnusson <lee@unhuman.io>
2022-06-16 07:04:56 -0700

    slow spi test

--- obot_g474/version.h
+++ obot_g474/version.h
@@ -2,7 +2,7 @@
 #define VERSION_H
 
 #define BUILD_DATETIME __DATE__ " " __TIME__
-#define GIT_HASH GIT_VERSION
+#define GIT_HASH GIT_VERSION NOTES
 #define PRODUCT_STRING "OBOT motor controller"
 #define USBD_PID (0x100)
 #define MANUFACTURER_STRING "Unhuman Inc."

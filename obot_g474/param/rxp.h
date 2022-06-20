cf114ec21cd9c157ae1f58c52f886819f18e5dff
Lee Magnusson <lee@unhuman.io>
2022-06-20 14:22:13 -0700

    update param

--- obot_g474/param/rxp.h
+++ obot_g474/param/rxp.h
@@ -1,6 +1,6 @@
 #define PARAM_OVERRIDES \
 .name = "right_hip_pitch", \
-.main_loop_param.output_encoder.bias = 2.126+.155, \
+.main_loop_param.output_encoder.bias = 2.126+.155-.003, \
 .fast_loop_param.motor_encoder.use_index_electrical_offset_pos = 1, \
 .fast_loop_param.motor_encoder.index_electrical_offset_pos = 4278 - 2778, /*-3149 - (-1373), */ \
 .startup_param.phase_lock_current = 20, \

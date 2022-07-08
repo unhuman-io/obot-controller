9e9e234c42a1253a6ab228a83a13117705f8253d
Lee Magnusson <lee@unhuman.io>
2022-07-08 04:40:27 -0700

    config and parameters for osa14 standalone

---
+++ obot_g474/param/a4.h
@@ -0,0 +1,3 @@
+#define PARAM_OVERRIDES \
+.name = "a4", \
+.main_loop_param.torque_sensor.bias = -28.9,
---
+++ obot_g474/param/a7.h
@@ -0,0 +1,3 @@
+#define PARAM_OVERRIDES \
+.name = "a7", \
+.main_loop_param.torque_sensor.bias = -28.4,

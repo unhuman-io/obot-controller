c8e2e32671112ca10a4dc5b3c631d36b9e9b366f
Lee Magnusson <leemagnusson@gmail.com>
2022-11-07 13:42:16 -0800

    qia128

--- obot_g474/config/config_obot_g474_motor_aksim.cpp
+++ obot_g474/config/config_obot_g474_motor_aksim.cpp
@@ -5,12 +5,19 @@
 #include "../../motorlib/torque_sensor.h"
 #include "../../motorlib/gpio.h"
 #include "../../motorlib/temperature_sensor.h"
+#include "../../motorlib/qia128.h"
+#include "../../motorlib/peripheral/stm32g4/pin_config.h"
+#include "../../motorlib/sensor_multiplex.h"
 
-using TorqueSensor = TorqueSensorBase;
+//using TorqueSensor = QIA128; 
+//using TorqueSensor = TorqueSensorBase;
 using MotorEncoder = Aksim2Encoder<18>;
 //using MotorEncoder = EncoderBase;
-using OutputEncoder = Aksim2Encoder<18>;
+//using OutputEncoder = Aksim2Encoder<18>;
 //using OutputEncoder = EncoderBase;
+
+using TorqueSensor = TorqueSensorMultiplex<QIA128, Aksim2Encoder<18>>;
+using OutputEncoder = TorqueSensor::SecondarySensor;
 
 extern "C" void SystemClock_Config();
 void pin_config_obot_g474_motor_r0();
@@ -26,6 +33,8 @@ struct InitCode {
       SPI1->CR2 = (7 << SPI_CR2_DS_Pos) | SPI_CR2_FRXTH;   // 8 bit
       // ORDER DEPENDANCE SPE set last
       SPI1->CR1 = SPI_CR1_MSTR | (5 << SPI_CR1_BR_Pos) | SPI_CR1_SSI | SPI_CR1_SSM | SPI_CR1_CPOL;    // baud = clock/64
+      SPI1->CR1 = SPI_CR1_MSTR | (6 << SPI_CR1_BR_Pos) | SPI_CR1_SSI | SPI_CR1_SSM;    // baud = clock/128 qia128
+      GPIO_SETH(B, 10, GPIO::OUTPUT, GPIO_SPEED::MEDIUM, 0);
       DMAMUX1_Channel0->CCR =  DMA_REQUEST_SPI3_TX;
       DMAMUX1_Channel1->CCR =  DMA_REQUEST_SPI3_RX;
       DMAMUX1_Channel2->CCR =  DMA_REQUEST_SPI1_TX;
@@ -40,12 +49,20 @@ namespace config {
 
     GPIO motor_encoder_cs(*GPIOD, 2, GPIO::OUTPUT);
     SPIDMA spi3_dma(*SPI3, motor_encoder_cs, *DMA1_Channel1, *DMA1_Channel2);
-    OutputEncoder output_encoder(spi3_dma);
+    MotorEncoder motor_encoder(spi3_dma);
     //EncoderBase motor_encoder;
-    TorqueSensor torque_sensor;
+    
     GPIO output_encoder_cs(*GPIOC, 3, GPIO::OUTPUT);
-    SPIDMA spi1_dma(*SPI1, output_encoder_cs, *DMA1_Channel3, *DMA1_Channel4);
-    MotorEncoder motor_encoder(spi1_dma);
+    GPIO torque_sensor_cs(*GPIOB, 10, GPIO::OUTPUT);
+    // SPIDMA spi1_dma(*SPI1, output_encoder_cs, *DMA1_Channel3, *DMA1_Channel4);
+    // SPIDMA spi1_dma2(*SPI1, torque_sensor_cs, *DMA1_Channel3, *DMA1_Channel4);
+    SPIDMA spi1_dma(*SPI1, torque_sensor_cs, *DMA1_Channel3, *DMA1_Channel4);
+    SPIDMA spi1_dma2(*SPI1, output_encoder_cs, *DMA1_Channel3, *DMA1_Channel4); // temporary switch
+    Aksim2Encoder<18> output_encoder_direct(spi1_dma);
+    QIA128 torque_sensor_direct(spi1_dma2);
+//    TorqueSensor torque_sensor;
+    TorqueSensor torque_sensor(torque_sensor_direct, output_encoder_direct, 5);
+    OutputEncoder &output_encoder = torque_sensor.secondary();
     //EncoderBase output_encoder;
 };
 

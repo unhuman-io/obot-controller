527b5192e9d75a8202614ac657b67117dc31f057
Jeff Bingham <bingjeff@gmail.com>
2023-03-26 18:06:49 -0700

    Format parameter files.

--- obot_g474/param/param_obot_g474_h2.h
+++ obot_g474/param/param_obot_g474_h2.h
@@ -4,10 +4,10 @@
 #define STR(x) #x
 
 typedef struct {
-    char name[64];    // name available through usb string
-    FastLoopParam fast_loop_param;
-    MainLoopParam main_loop_param;
-    StartupParam startup_param;
+  char name[64];  // name available through usb string
+  FastLoopParam fast_loop_param;
+  MainLoopParam main_loop_param;
+  StartupParam startup_param;
 } Param;
 
-extern const Param * const param;
+extern const Param* const param;

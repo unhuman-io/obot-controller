c3869677d911a89f986d27a45860ee93c277c6ce
Lee Magnusson <lee@unhuman.io>
2023-04-28 16:05:01 -0700

    wip 3 aksims

---
+++ obot_g474/param/param_obot_g474_aksim.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "../../motorlib/messages.h"
+#define XSTR(x) STR(x)
+#define STR(x) #x
+
+typedef struct {
+    char name[64];    // name available through usb string
+    FastLoopParam fast_loop_param;
+    MainLoopParam main_loop_param;
+    StartupParam startup_param;
+    uint16_t drv_regs[5];
+    float joint_encoder_bias;
+    float joint_encoder_rollover;
+} Param;
+
+extern const Param * const param;

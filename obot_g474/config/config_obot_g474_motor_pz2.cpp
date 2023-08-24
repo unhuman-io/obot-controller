60999c1979a0e68b19bf6cb0e4a94d46eb49f66b
Lee Magnusson <lee@unhuman.io>
2023-08-24 11:28:14 -0700

    Add encoder warnings

--- obot_g474/config/config_obot_g474_motor_pz2.cpp
+++ obot_g474/config/config_obot_g474_motor_pz2.cpp
@@ -1,5 +1,5 @@
 #include "../../motorlib/boards/config_obot_g474_motor.h"
-#include "../param/param_obot_g474.h"
+#include "../param/param_obot_g474_pz2.h"
 #include "../st_device.h"
 #include "../../motorlib/peripheral/stm32g4/spi_dma.h"
 #include "../../motorlib/icpz.h"

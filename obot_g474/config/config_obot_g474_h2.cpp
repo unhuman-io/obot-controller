67f9134f8dd63f30bdfd030d9698c9491287a195
Ernesto Urdaneta <ernesto.urdaneta@figure.ai>
2023-09-05 11:31:12 -0700

    Moons motor Biss encoder added

--- obot_g474/config/config_obot_g474_h2.cpp
+++ obot_g474/config/config_obot_g474_h2.cpp
@@ -2,11 +2,20 @@
 #include "../st_device.h"
 #include "../../motorlib/torque_sensor.h"
 #include "../../motorlib/gpio.h"
+#include "../../motorlib/hall.h"
 #include "../../motorlib/ma782_encoder.h"
 #include "../../motorlib/peripheral/stm32g4/pin_config.h"
+#include "../../motorlib/peripheral/stm32g4/spi_dma.h"
+#include "../../motorlib/moons_encoder.h"
+
+#ifndef MOTOR_ENCODER_BITS
+#define MOTOR_ENCODER_BITS 12
+#endif
 
 using TorqueSensor = TorqueSensorBase;
-using MotorEncoder = MA782Encoder;
+// using MotorEncoder = MA782Encoder;
+// using MotorEncoder = HallEncoder;
+using MotorEncoder = MoonsEncoder<MOTOR_ENCODER_BITS>;
 using OutputEncoder = EncoderBase;
 
 extern "C" void SystemClock_Config();
@@ -19,22 +28,48 @@ struct InitCode
         SystemClock_Config();
 
         pin_config_obot_g474_h2();
-        // SPI1 MA782
-        SPI1->CR2 = (15 << SPI_CR2_DS_Pos); // 16 bit
+
+        // Moons BiSS motor encoder on SPI3
+        SPI3->CR2 = (7 << SPI_CR2_DS_Pos) | SPI_CR2_FRXTH; // 8 bit
         // ORDER DEPENDANCE SPE set last
-        SPI1->CR1 = SPI_CR1_MSTR | (3 << SPI_CR1_BR_Pos) | SPI_CR1_SSI | SPI_CR1_SSM | SPI_CR1_SPE; // baud = clock/16
+        SPI3->CR1 = SPI_CR1_MSTR | (4 << SPI_CR1_BR_Pos) | SPI_CR1_SSI | SPI_CR1_SSM | SPI_CR1_CPOL;    // baud = clock/32
+
+        DMAMUX1_Channel0->CCR =  DMA_REQUEST_SPI3_TX;
+        DMAMUX1_Channel1->CCR =  DMA_REQUEST_SPI3_RX;
+        GPIO_SETH(A, 15, GPIO_MODE::OUTPUT, GPIO_SPEED::VERY_HIGH, 0);   // PA15-> motor encoder cs
+        
+        // SPI1 MA782
+        // SPI1->CR2 = (15 << SPI_CR2_DS_Pos); // 16 bit
+        // // ORDER DEPENDANCE SPE set last
+        // SPI1->CR1 = SPI_CR1_MSTR | (3 << SPI_CR1_BR_Pos) | SPI_CR1_SSI | SPI_CR1_SSM | SPI_CR1_SPE; // baud = clock/16
+
+        // Setup Hall Inputs
+        // GPIO_SETL(B, 3, GPIO::INPUT, GPIO_SPEED::VERY_HIGH, 0);
+        // GPIO_SETL(B, 4, GPIO::INPUT, GPIO_SPEED::VERY_HIGH, 0);
+        // GPIO_SETL(B, 5, GPIO::INPUT, GPIO_SPEED::VERY_HIGH, 0);
     }
 };
 
 namespace config
 {
     const uint32_t main_loop_frequency = 10000;
-    const uint32_t pwm_frequency = 50000;
+    const uint32_t pwm_frequency = 40000;
     InitCode init_code;
 
-    GPIO motor_encoder_cs = {*GPIOA, 4, GPIO::OUTPUT};
+    // GPIO motor_encoder_cs = {*GPIOA, 4, GPIO::OUTPUT};
 
-    MA782Encoder motor_encoder(*SPI1, motor_encoder_cs);
+    // MA782Encoder motor_encoder(*SPI1, motor_encoder_cs);
+
+    // Hall Encoder
+    // GPIO gpio_a(*GPIOB, 3);
+    // GPIO gpio_b(*GPIOB, 4);
+    // GPIO gpio_c(*GPIOB, 5);
+    // HallEncoder motor_encoder(gpio_a, gpio_b, gpio_c);
+    
+    // Moons motor BiSS encoder
+    GPIO motor_encoder_cs(*GPIOA, 15, GPIO::OUTPUT);
+    SPIDMA spi3_dma(*SPI3, motor_encoder_cs, *DMA1_Channel1, *DMA1_Channel2);
+    MotorEncoder motor_encoder(spi3_dma);
 
     TorqueSensor torque_sensor;
 
@@ -59,7 +94,7 @@ volatile uint32_t *const cpu_clock = &DWT->CYCCNT;
 #include "../../motorlib/controller/velocity_controller.h"
 #include "../../motorlib/controller/state_controller.h"
 #include "../../motorlib/controller/joint_position_controller.h"
-#include "../../motorlib/controller/admittance_controller.h"
+// #include "../../motorlib/controller/admittance_controller.h"
 #include "../../motorlib/fast_loop.h"
 #include "../../motorlib/main_loop.h"
 #include "../../motorlib/actuator.h"
@@ -90,8 +125,9 @@ namespace config
     VelocityController velocity_controller = {(float)(1.0 / main_loop_frequency)};
     StateController state_controller = {(float)(1.0 / main_loop_frequency)};
     JointPositionController joint_position_controller(1.0 / main_loop_frequency);
-    AdmittanceController admittance_controller = {1.0/main_loop_frequency};
-    MainLoop main_loop(main_loop_frequency, fast_loop, position_controller, torque_controller, impedance_controller, velocity_controller, state_controller, joint_position_controller, admittance_controller, System::communication_, led, output_encoder, torque_sensor, driver, param->main_loop_param);
+    // AdmittanceController admittance_controller = {1.0/main_loop_frequency};
+    // MainLoop main_loop(main_loop_frequency, fast_loop, position_controller, torque_controller, impedance_controller, velocity_controller, state_controller, joint_position_controller, System::communication_, led, output_encoder, torque_sensor, driver, param->main_loop_param);
+    MainLoop main_loop(fast_loop, position_controller, torque_controller, impedance_controller, velocity_controller, state_controller, joint_position_controller, System::communication_, led, output_encoder, torque_sensor, driver, param->main_loop_param);
 };
 
 Communication System::communication_ = {config::usb};
@@ -275,15 +311,24 @@ void finish_sleep()
 
 void config_init()
 {
-    System::api.add_api_variable("mbct", new APICallbackUint32([](){ return config::motor_encoder.get_bct(); },
-                    [](uint32_t u){ config::motor_encoder.set_bct(u); }));
-    System::api.add_api_variable("met", new APICallbackUint32([](){ return config::motor_encoder.get_et(); },
-                    [](uint32_t u){ config::motor_encoder.set_et(u); }));
-    System::api.add_api_variable("mmgt", new APICallbackUint32([](){ return config::motor_encoder.get_magnetic_field_strength(); },
-                    [](uint32_t u){ config::motor_encoder.set_mgt(u); }));
-    System::api.add_api_variable("mfilt", new APICallbackUint32([](){ return config::motor_encoder.get_filt(); }, 
-                [](uint32_t u){ config::motor_encoder.set_filt(u); }));
+    // System::api.add_api_variable("mbct", new APICallbackUint32([](){ return config::motor_encoder.get_bct(); },
+    //                 [](uint32_t u){ config::motor_encoder.set_bct(u); }));
+    // System::api.add_api_variable("met", new APICallbackUint32([](){ return config::motor_encoder.get_et(); },
+    //                 [](uint32_t u){ config::motor_encoder.set_et(u); }));
+    // System::api.add_api_variable("mmgt", new APICallbackUint32([](){ return config::motor_encoder.get_magnetic_field_strength(); },
+    //                 [](uint32_t u){ config::motor_encoder.set_mgt(u); }));
+    // System::api.add_api_variable("mfilt", new APICallbackUint32([](){ return config::motor_encoder.get_filt(); }, 
+    //             [](uint32_t u){ config::motor_encoder.set_filt(u); }));
     System::api.add_api_variable("3v3_bus", new APIUint32(&V_3V3_BUS));
+    System::api.add_api_variable("mdiag", new const APIUint8(&config::motor_encoder.status_.word));
+    System::api.add_api_variable("mdiag_raw", new const APIUint8(&config::motor_encoder.diag_raw_.word));
+    System::api.add_api_variable("mcrc", new const APIUint8(&config::motor_encoder.crc_calc_));
+    System::api.add_api_variable("merr", new APIUint32(&config::motor_encoder.diag_err_count_));
+    System::api.add_api_variable("mwarn", new APIUint32(&config::motor_encoder.diag_warn_count_));
+    System::api.add_api_variable("mcrc_cnt", new APIUint32(&config::motor_encoder.crc_err_count_));
+    System::api.add_api_variable("mraw", new APIUint32(&config::motor_encoder.raw_value_));
+    System::api.add_api_variable("mrawh", new const APICallback([](){ return u32_to_hex(config::motor_encoder.raw_value_); }));
+    System::api.add_api_variable("mcrc_latch", new const APIUint32(&config::motor_encoder.crc_error_raw_latch_));
 
 
     // System::api.add_api_variable("torque1", new const APIFloat(&config::torque_sensor.torque1_));
@@ -291,4 +336,11 @@ void config_init()
     // System::api.add_api_variable("torque_decimation", new APIUint16(&config::torque_sensor.decimation_));
 }
 
-void config_maintenance() {}
+void config_maintenance() {
+    if(config::motor_encoder.crc_err_count_ > 100 || config::motor_encoder.diag_err_count_ > 100 ||
+        config::motor_encoder.diag_warn_count_ > pow(2,31)) {
+            config::main_loop.status_.error.motor_encoder = true;
+    }
+    round_robin_logger.log_data(MOTOR_ENCODER_CRC_INDEX, config::motor_encoder.crc_err_count_);
+    // round_robin_logger.log_data(MOTOR_ENCODER_ERROR_INDEX, config::motor_encoder.diag_err_count_);
+}

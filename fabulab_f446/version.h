a91d7b1e8e7530684593c943fc95f5f71899b9fe
Lee Magnusson <lee@unhuman.io>
2023-09-06 13:16:15 -0700

    git hash in make for other projects

--- fabulab_f446/version.h
+++ fabulab_f446/version.h
@@ -2,7 +2,6 @@
 #define VERSION_H
 
 #define BUILD_DATETIME __DATE__ " " __TIME__
-#define GIT_HASH GIT_VERSION
 #define PRODUCT_STRING "Fabulab F446"
 #define USBD_PID (0x100)
 #define MANUFACTURER_STRING "Fabulab"

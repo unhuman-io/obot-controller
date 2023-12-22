79abf00dc064b71767f92cd7d12d0e9aa8956d15
leemagnusson <leemagnusson@gmail.com>
2023-12-22 05:33:55 -0800

    Feature/api string view (#198)
    * c++17
    
    * debug vars to macro
    
    * runtime api constness check
    
    ---------
    
    Co-authored-by: Lee Magnusson <lee@unhuman.io>

--- fabulab_f446/st_device.h
+++ fabulab_f446/st_device.h
@@ -5,4 +5,10 @@
 #include "stm32f4xx_hal.h"
 #define CPU_FREQUENCY_HZ    180e6
 
+#ifdef __cplusplus
+inline bool is_rom(void * p) {
+    return (uint32_t) p >= FLASH_BASE && (uint32_t) p <= FLASH_END;
+}
+#endif
+
 #endif

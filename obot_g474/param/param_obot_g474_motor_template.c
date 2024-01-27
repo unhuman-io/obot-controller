e5daec8b02f9285c3be0100ed531f7af31cb8f6e
Lee Magnusson <lee@unhuman.io>
2024-01-27 06:53:38 -0800

    build a uart

---
+++ obot_g474/param/param_obot_g474_motor_template.c
@@ -0,0 +1,13 @@
+#include "param_obot_g474.h"
+#include "math.h"
+
+
+// Can be written by external methods, e.g. bootloader
+const volatile Param __attribute__ ((section ("flash_param"))) param_store = {
+.name = "template",
+};
+const volatile char * const name = param_store.name;
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdiscarded-qualifiers"
+const Param * const param = &param_store; // todo figure out a way to not inline without warning
+#pragma GCC diagnostic pop

2ddb2b3eb4bd2c1c944bb8e18d168f366d937f9d
leemagnusson <leemagnusson@gmail.com>
2024-03-07 16:21:29 -0800

    nucleo spi build (#221)
    Co-authored-by: Lee Magnusson <lee@unhuman.io>

---
+++ obot_g474/config/config_obot_g474_nucleo_spi.cpp
@@ -0,0 +1,33 @@
+#include "../param/param_obot_g474.h"
+#include "st_device.h"
+#include "../../motorlib/encoder.h"
+#include "../../motorlib/torque_sensor.h"
+#include "../../motorlib/gpio.h"
+#include <algorithm>
+#include "../../motorlib/peripheral/stm32g4/pin_config.h"
+#define COMMS   COMMS_SPI
+
+using TorqueSensor = TorqueSensorBase;
+using MotorEncoder = EncoderBase;
+using OutputEncoder = EncoderBase;
+
+struct InitCode {
+    InitCode() {
+    }
+};
+
+namespace config {
+    const uint32_t main_loop_frequency = 10000;    
+    const uint32_t pwm_frequency = 50000;
+    InitCode init_code;
+
+    MotorEncoder motor_encoder;
+    TorqueSensor torque_sensor;
+    OutputEncoder output_encoder;
+};
+
+#include "../../motorlib/boards/config_obot_g474_motor.cpp"
+
+void config_init() {}
+
+void config_maintenance() {}

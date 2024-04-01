e415562392e33ddad7746525b4aac8f1e8c79c30
Lee Magnusson <lee@unhuman.io>
2024-04-01 09:49:34 -0700

    timing project

---
+++ obot_g474/param/param_obot_g474_motor_timing.c
@@ -0,0 +1,9 @@
+#include "param_obot_g474.h"
+#include "math.h"
+
+
+// Can be written by external methods, e.g. bootloader
+const volatile Param __attribute__ ((section ("flash_param"))) param_store = {
+#include "param_default.h"
+.name = "timing",
+};

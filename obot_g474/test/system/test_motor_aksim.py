965212d3376a444c0362f4c07324f170cc66500c
Lee Magnusson <lee@unhuman.io>
2024-05-24 13:09:19 -0700

    Add imu test

---
+++ obot_g474/test/system/test_motor_aksim.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import motor
+import time
+import unittest
+import sys
+import numpy as np
+
+path = None
+class TestMotor(unittest.TestCase):
+    m = None
+    motor = None
+
+    @classmethod
+    def setUpClass(cls):
+        cls.m = motor.MotorManager()
+        if (path):
+            cls.m.get_motors_by_path([path])
+        cls.motor = cls.m.motors()[0]
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.m.set_command_mode(motor.ModeDesired.Open)
+        cls.m.write_saved_commands()
+
+    def tearDown(self):
+        self.m.set_commands([motor.Command()])
+        self.m.set_command_mode(motor.ModeDesired.Open)
+        self.m.write_saved_commands()
+    
+    def test_imu(self):
+        self.motor["imu_read"].get()
+        time.sleep(0.01)
+        val = self.motor["ax"].get()
+        print("ax: " + val)
+        self.assertAlmostEqual(float(val), -1.0, 1)
+
+
+if __name__ == "__main__":
+    if(len(sys.argv) > 1 and sys.argv[1] == '-p'):
+        path = sys.argv[2]
+        del(sys.argv[1:3])
+    unittest.main()

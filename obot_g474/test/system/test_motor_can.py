95ab046903f6b5524f43d81262cfa4e9a07954f0
leemagnusson <leemagnusson@gmail.com>
2024-11-21 10:45:40 -0800

    Feature/api timeout request (#298)
    * api timeout request
    
    * merge
    
    * more timeout
    
    * long packet
    
    * fix uart communication
    
    * test long packets
    
    * reorder test
    
    * fix stack size
    
    * test can fail
    
    * run motor tests
    
    * don't test uart long packet
    
    ---------
    
    Co-authored-by: Lee Magnusson <lee@unhuman.io>

---
+++ obot_g474/test/system/test_motor_can.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import motor
+import unittest
+
+path = None
+class TestMotor(unittest.TestCase):
+    m = None
+    f = None
+
+    @classmethod
+    def connect(cls):
+        cls.m = motor.MotorManager()
+        cls.m.get_motors_can(["can0:1"])
+        cls.m.set_auto_count()
+
+    @classmethod
+    def setUpClass(cls):
+        cls.connect()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.m.clear_commands()
+        cls.m.set_command_mode(motor.ModeDesired.Open)
+        cls.m.write_saved_commands()
+
+    def tearDown(self):
+        self.m.clear_commands()
+        self.m.set_command_mode(motor.ModeDesired.Open)
+        self.m.write_saved_commands()
+
+    def test_really_long_packet(self):
+        packet = self.m.motors()[0]["really_long_packet"].get()
+        self.assertEqual(len(packet), motor.max_api_packet_size())
+        expected_packet = "0123456789" * (motor.max_api_packet_size() // 10)
+        self.assertEqual(packet, expected_packet)
+
+if __name__ == "__main__":
+    unittest.main()

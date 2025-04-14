4627d519e9831273e67d9183a73ea6074d339be3
Lee Magnusson <lee@unhuman.io>
2025-04-14 12:28:19 -0700

    trace to new parameter alloc

--- scripts/new_to_static.py
+++ scripts/new_to_static.py
@@ -5,17 +5,17 @@ import re
 
 with open(sys.argv[1], 'r') as f:
     data = f.read()
-    data = re.sub(r'^(\s*)api\.add_api_variable\(\"(\w+)\", new (const )?(API[\w<>]+)\(([^\{\}]+?)\)\);\s*$',
-                   r'\1api.add_api_variable<\3\4>("\2", \5);', data, flags=re.MULTILINE | re.DOTALL)
+    data = re.sub(r'^([\s\w:]*)api\.add_api_variable\(\"(\w+)\", new (const )?(API[\w<>]+)\(([^\{\}]+?)\)\);\s*(\\)$',
+                   r'\1api.add_api_variable<\3\4>("\2", \5);\6', data, flags=re.MULTILINE | re.DOTALL)
 
-    data = re.sub(r'^(\s*)api\.add_api_variable\(\"(\w+)\", new (const )?(API[\w<>]+)\((.+?\}.+?)\)+;\s*$',
-                   r'\1api.add_api_variable<\3\4>("\2", \5);', data, flags=re.MULTILINE | re.DOTALL)
+    data = re.sub(r'^([\s\w:]*)api\.add_api_variable\(\"(\w+)\", new (const )?(API[\w<>]+)\((.+?\}.+?)\)+;\s*(\\)$',
+                   r'\1api.add_api_variable<\3\4>("\2", \5);\6', data, flags=re.MULTILINE | re.DOTALL)
     print(data)
 
     #match = re.findall(r'^(\s*)api.add_api_variable\(\"(\w+)\", new (const )?(API[\w<>]+)(.+?\{.+?\}.+?)\);\s*$', data, re.MULTILINE | re.DOTALL)
     # for m in match:
     #     print(m)
-    # for m in match:
+    # for m in match:ueuqeuqejj
     #    if m.group('lambda'):
     #         print(m)
         #print(f"{m[0]}static {m[2]}{m[3]} api_{m[1]}{m[4]};")

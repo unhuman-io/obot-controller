67861a178c434c3f1871aa33bb2304f84a0ebd87
Lee Magnusson <lee@unhuman.io>
2025-05-07 15:45:01 -0700

    trace as a module

--- obot_g474/config/config_obot_g474_trace2.cpp
+++ obot_g474/config/config_obot_g474_trace2.cpp
@@ -5,15 +5,17 @@
 #include "../../motorlib/gpio.h"
 #include <algorithm>
 #include "../../motorlib/peripheral/stm32g4/pin_config.h"
-#define COMMS   COMMS_USB
+
 
 using TorqueSensor = TorqueSensorBase;
 using MotorEncoder = EncoderBase;
 using OutputEncoder = EncoderBase;
+#include "../../motorlib/logger.h"
+import config_obot_g474_tracec;
 
-
-struct InitCode {
-    InitCode() {
+class Trace2 : public TraceBoard {
+public:
+    Trace2() {
         // trace pins enable
         RCC->AHB2ENR |= RCC_AHB2ENR_GPIOEEN;
         GPIO_SETL(E, 2, GPIO_MODE::ALT_FUN, GPIO_SPEED::VERY_HIGH, 0);
@@ -26,23 +28,29 @@ struct InitCode {
         *etmcr |= 1 << 11;
         uint32_t *etmteevr = (uint32_t *)0xE0041020;
         *etmteevr = 0x000037ef; // ON
-
     }
-};
 
-namespace config {
     const uint32_t main_loop_frequency = 10000;    
     const uint32_t pwm_frequency = 50000;
-    InitCode init_code;
 
     MotorEncoder motor_encoder;
     TorqueSensor torque_sensor;
     OutputEncoder output_encoder;
+
 };
 
-import config_obot_g474_traceb;
-//#include "../../motorlib/boards/config_obot_g474_traceb.cpp"
+Trace2 trace2;
 
+Logger::CIndex log_index __attribute__((section(".noload")));
+char log_queue[LOGGING_MAX_SIZE] __attribute__((section(".noload")));
+__attribute__ ((init_priority(LOGGER_INIT_PRIORITY))) Logger logger(log_index, log_queue);
+extern const char * const name = "obot_g474_trace2";
+
+extern "C" void usb_interrupt() {
+    trace2.usb.interrupt();
+}
 void config_init() {}
 
 void config_maintenance() {}
+
+extern "C" void board_init() {}

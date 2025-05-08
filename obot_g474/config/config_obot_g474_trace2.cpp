43161bd5386bb9b81d12e838dba8afcad9430741
Lee Magnusson <lee@unhuman.io>
2025-05-08 05:28:40 -0700

    trace2 builds

--- obot_g474/config/config_obot_g474_trace2.cpp
+++ obot_g474/config/config_obot_g474_trace2.cpp
@@ -6,16 +6,33 @@
 #include <algorithm>
 #include "../../motorlib/peripheral/stm32g4/pin_config.h"
 
+#include "../controller/position_controller.h"
+#include "../controller/torque_controller.h"
+#include "../controller/impedance_controller.h"
+#include "../controller/velocity_controller.h"
+#include "../controller/state_controller.h"
+#include "../controller/joint_position_controller.h"
+#include "../controller/admittance_controller.h"
+
 
 using TorqueSensor = TorqueSensorBase;
 using MotorEncoder = EncoderBase;
 using OutputEncoder = EncoderBase;
 #include "../../motorlib/logger.h"
 import config_obot_g474_tracec;
+import system2;
+import fast_loop;
+import main_loop;
+
+const Param * const param = (const Param * const) 0x8060000;
+const Calibration * const calibration = (const Calibration * const) 0x8070000;
+
+constexpr uint32_t main_loop_frequency = 10000;    
+constexpr uint32_t pwm_frequency = 50000;
 
 class Trace2 : public TraceBoard {
 public:
-    Trace2() {
+    Trace2() : TraceBoard(pwm_frequency, main_loop_frequency){
         // trace pins enable
         RCC->AHB2ENR |= RCC_AHB2ENR_GPIOEEN;
         GPIO_SETL(E, 2, GPIO_MODE::ALT_FUN, GPIO_SPEED::VERY_HIGH, 0);
@@ -30,13 +47,50 @@ public:
         *etmteevr = 0x000037ef; // ON
     }
 
-    const uint32_t main_loop_frequency = 10000;    
-    const uint32_t pwm_frequency = 50000;
+
 
     MotorEncoder motor_encoder;
     TorqueSensor torque_sensor;
     OutputEncoder output_encoder;
 
+    FastLoop<PWM, MotorEncoder, Calibration> fast_loop = {(int32_t) pwm_frequency, motor_pwm, motor_encoder, param->fast_loop_param, *calibration, &I_A_DR, &I_B_DR, &I_C_DR, &V_BUS_DR};
+
+    PositionController position_controller = {(float) (1.0/main_loop_frequency)};
+    TorqueController torque_controller = {(float) (1.0/main_loop_frequency)};
+    ImpedanceController impedance_controller = {(float) (1.0/main_loop_frequency)};
+    VelocityController velocity_controller = {(float) (1.0/main_loop_frequency)};
+    StateController state_controller = {(float) (1.0/main_loop_frequency)};
+    JointPositionController joint_position_controller{1.0/main_loop_frequency};
+    AdmittanceController admittance_controller = {1.0/main_loop_frequency};
+    MainLoop<FastLoop<PWM, MotorEncoder, Calibration>,
+        Driver,
+        PositionController,
+        TorqueController,
+        ImpedanceController,
+        VelocityController,
+        StateController,
+        JointPositionController,
+        AdmittanceController,
+        Communication,
+        LED,
+        OutputEncoder,
+        TorqueSensor>
+        main_loop = {main_loop_frequency,
+            fast_loop,
+            position_controller,
+            torque_controller,
+            impedance_controller,
+            velocity_controller,
+            state_controller,
+            joint_position_controller,
+            admittance_controller,
+            communication,
+            led,
+            output_encoder,
+            torque_sensor,
+            drv,
+            param->main_loop_param,
+            *calibration};
 };
 
 Trace2 trace2;
@@ -54,3 +108,23 @@ void config_init() {}
 void config_maintenance() {}
 
 extern "C" void board_init() {}
+
+extern "C" void system_run() {
+    //System<>::run();
+}
+
+extern "C" void main_loop_interrupt() {
+ //   System::main_loop_interrupt();
+}
+
+extern "C" void fast_loop_interrupt() {
+  //  System::fast_loop_interrupt();
+}
+
+extern "C" void system_log(std::string s) {
+ //   System::log(s);
+}
+
+extern "C" void system_loop_interrupt() {
+  //  System::system_loop();
+}

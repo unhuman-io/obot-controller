593a01d6352bf2d1f0742f38050ce8c2f87c4eb5
Lee Magnusson <lee@unhuman.io>
2025-10-05 06:30:54 -0700

    llvm in toolchain

--- obot2/make/toolchain.mk
+++ obot2/make/toolchain.mk
@@ -1,19 +1,23 @@
 ifneq ($(USE_LLVM),)
 $(info LLVM)
-	LLVM_PATH=../motorlib/llvm/bin/
+	LLVM_PATH=../../motorlib/llvm/bin/
 	CXX = $(LLVM_PATH)clang++
 	CP = $(LLVM_PATH)llvm-objcopy
 	CPPFLAGS += --target=thumbv7em-none-eabi
-	CXXFLAGS += -fprebuilt-module-path=.
+	CXXFLAGS += -fprebuilt-module-path=. $(addprefix -fmodule-file=,$(c++_header_units))
 	CXXMFLAGS += -fmodule-output -x c++-module -Wno-experimental-header-units
 	LDFLAGS += -nostartfiles
+	c++_header_units = $(c++_header_modules:%=%.pcm)
 else
 $(info GCC)
-	CC = $(GCC_PATH)arm-none-eabi-gcc
 	CXX = $(GCC_PATH)arm-none-eabi-g++
 	CP = $(GCC_PATH)arm-none-eabi-objcopy
-	CXXFLAGS += -fmodules-ts
+	CXXFLAGS += -fmodules-ts -Mno-modules
+	CXXMFLAGS += -x c++
 	LDFLAGS += -specs=nosys.specs
+	c++_headers_location := $(shell realpath `$(CXX) --print-sysroot`)/include/c++/$(shell $(CXX) -dumpversion)
+$(info c++ headers location: $(c++_headers_location))
+	c++_header_units = $(addprefix gcm.cache/.$(c++_headers_location)/, $(c++_header_modules:%=%.gcm))
 endif
 
 C_INCLUDES += -I$(SELF_DIR)../motorlib/CMSIS/Include -I$(SELF_DIR)../motorlib/device/stm32g4/Include

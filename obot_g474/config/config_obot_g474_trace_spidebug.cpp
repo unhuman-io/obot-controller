7d4acf62fe2e71b6ca1d5ab0e27d1726ff0c6b8d
Lee Magnusson <lee@unhuman.io>
2025-12-08 14:54:47 -0800

    different spidebug

--- obot_g474/config/config_obot_g474_trace_spidebug.cpp
+++ obot_g474/config/config_obot_g474_trace_spidebug.cpp
@@ -8,15 +8,11 @@
 #include "../../motorlib/peripheral/stm32g4/spi_dma.h"
 #include "../../motorlib/peripheral/stm32g4/spi_debug.h"
 #include "../../motorlib/gpio.h"
-#include "../../motorlib/sensors/encoders/stm32g4/a17803.h"
-#include "../../motorlib/sensors/encoders/stm32g4/a17803_dma.h"
 
 #define COMMS   COMMS_USB
 
 using TorqueSensor = TorqueSensorBase;
-//using MotorEncoder = A17803;
-//using MotorEncoder = EncoderBase;
-using MotorEncoder = A17803_DMA<SPIDMA>;
+using MotorEncoder = EncoderBase;
 using OutputEncoder = EncoderBase;
 
 struct InitCode {
@@ -38,45 +34,23 @@ struct InitCode {
         DMAMUX1_Channel1->CCR =  DMA_REQUEST_SPI1_RX;
 
         GPIO_SETL(A, 3, GPIO_MODE::OUTPUT, GPIO_SPEED::MEDIUM, 0); // CS2
-        GPIO_SETL(A, 0, GPIO_MODE::OUTPUT, GPIO_SPEED::MEDIUM, 0); // dummy
-        GPIOA->BSRR = GPIO_BSRR_BS3 | GPIO_BSRR_BS0 | GPIO_BSRR_BS4;
-        MASK_SET(SYSCFG->EXTICR[0], SYSCFG_EXTICR1_EXTI0, 0); // EXTI PA0
-
-        HRTIM1->sTimerxRegs[0].CMP1xR = 100;
-        HRTIM1->sTimerxRegs[0].CMP2xR = 45333*.33;
-        HRTIM1->sTimerxRegs[0].CMP3xR = 45333*.66;
-        HRTIM1->sTimerxRegs[0].TIMxCR2 = 0;// doesn't matter 2 << HRTIM_TIMCR2_ROM_Pos; // on period
-        HRTIM1->sTimerxRegs[0].PERxR = 45333;
-        HRTIM1->sTimerxRegs[0].TIMxCR |= HRTIM_TIMCR_PREEN | HRTIM_TIMCR_TRSTU | HRTIM_TIMCR_CONT | 2 << HRTIM_TIMCR_CK_PSC_Pos;
-
-        DMAMUX1_Channel2->CCR = 96; // hrtima
-        DMAMUX1_Channel3->CCR = 1; // reqgen0
-        DMAMUX1_RequestGenerator0->RGCR = 1 << DMAMUX_RGxCR_GPOL_Pos | 17 << DMAMUX_RGxCR_SIG_ID_Pos | DMAMUX_RGxCR_GE;
-
-        RCC->AHB1ENR |= RCC_AHB1ENR_CRCEN; // enable CRC peripheral
-
+        GPIO_SETL(A, 2, GPIO_MODE::OUTPUT, GPIO_SPEED::MEDIUM, 0); // CS3
+        GPIOA->BSRR = GPIO_BSRR_BS2 | GPIO_BSRR_BS3 | GPIO_BSRR_BS4;
     }
 };
 
-void start_cs_trigger() {
-    HRTIM1->sTimerxRegs[0].TIMxDIER = HRTIM_TIMDIER_CMP1DE |  HRTIM_TIMDIER_CMP2DE |  HRTIM_TIMDIER_CMP3DE ;
-}
-void stop_cs_trigger() {
-    HRTIM1->sTimerxRegs[0].TIMxDIER = 0;
-    // wait for CS high
-    while(!(GPIOA->IDR & 0x0001));
-}
-
 namespace config {
     const uint32_t main_loop_frequency = 10000;    
-    const uint32_t pwm_frequency = 30000;
+    const uint32_t pwm_frequency = 50000;
     InitCode init_code;
 
     TorqueSensor torque_sensor;
+    MotorEncoder motor_encoder;
     OutputEncoder output_encoder;
 
     GPIO cs1 {*GPIOA, 4, GPIO::OUTPUT};
     GPIO cs2 {*GPIOA, 3, GPIO::OUTPUT};
+    GPIO cs3 {*GPIOA, 2, GPIO::OUTPUT};
     // CLK A4
     // MISO A5
     // MOSI A6
@@ -84,20 +58,19 @@ namespace config {
         SPI_CR1_MSTR | (4 << SPI_CR1_BR_Pos) | SPI_CR1_SSI | SPI_CR1_SSM | SPI_CR1_CPHA | SPI_CR1_CPOL, 200};
     SPIDMA spi_dma2 {SPIDMA::SP1, cs2, DMA1_CH1, DMA1_CH2, 0, 50, 50,
         SPI_CR1_MSTR | (4 << SPI_CR1_BR_Pos) | SPI_CR1_SSI | SPI_CR1_SSM | SPI_CR1_CPHA | SPI_CR1_CPOL, 200};
-    SPIDebug spi_debug(spi_dma1);
-
-    //A17803_DMA a17803_dma(spi_dma1, spi_dma2);
-    //A17803 encoder(spi_dma1);
-    //MotorEncoder motor_encoder(spi_dma1);
-    //MotorEncoder motor_encoder;
-    MotorEncoder motor_encoder(spi_dma1, spi_dma2, start_cs_trigger, stop_cs_trigger);
+    SPIDMA spi_dma3 {SPIDMA::SP1, cs3, DMA1_CH1, DMA1_CH2, 0, 50, 50,
+        SPI_CR1_MSTR | (4 << SPI_CR1_BR_Pos) | SPI_CR1_SSI | SPI_CR1_SSM | SPI_CR1_CPHA | SPI_CR1_CPOL, 200};
+    SPIDebug spi_debug1(spi_dma1);
+    SPIDebug spi_debug2(spi_dma2);
+    SPIDebug spi_debug3(spi_dma3);
 };
 
 #include "../../motorlib/boards/config_obot_g474_trace.cpp"
 
 void config_init() {
-    SPIDEBUG_SET_DEBUG_API(, System::api, config::spi_debug);
-    A17803_DMA_SET_DEBUG_API(, System::api, config::motor_encoder);
+    SPIDEBUG_SET_DEBUG_API("1", System::api, config::spi_debug1);
+    SPIDEBUG_SET_DEBUG_API("2", System::api, config::spi_debug2);
+    SPIDEBUG_SET_DEBUG_API("3", System::api, config::spi_debug3);
 }
 
 void config_maintenance() {}

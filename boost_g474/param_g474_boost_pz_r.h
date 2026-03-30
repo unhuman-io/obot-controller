d70ae58fdaca41c7da43b900c108a25b263e23c4
leemagnusson <leemagnusson@gmail.com>
2026-03-30 12:32:46 -0700

    remove boost and fabulab (#343)
    Co-authored-by: Lee Magnusson <lee@unhuman.io>

--- boost_g474/param_g474_boost_pz_r.h
+++
@@ -1,4 +0,0 @@
-#define PARAM_OVERRIDES \
-.name = "wr", \
-.fast_loop_param.motor_encoder.dir = 1, \
-.startup_param.phase_lock_current = -5,
--- fabulab_f446/version.h
+++
@@ -1,9 +0,0 @@
-#ifndef VERSION_H
-#define VERSION_H
-
-#define BUILD_DATETIME __DATE__ " " __TIME__
-#define PRODUCT_STRING "Fabulab F446"
-#define USBD_PID (0x100)
-#define MANUFACTURER_STRING "Fabulab"
-
-#endif

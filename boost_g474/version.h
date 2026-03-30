e1e7d553f474141092c29634da9e53ce620a358d
Lee Magnusson <lee@unhuman.io>
2026-03-30 11:51:52 -0700

    remove boost and fabulab

--- boost_g474/version.h
+++
@@ -1,9 +0,0 @@
-#ifndef VERSION_H
-#define VERSION_H
-
-#define BUILD_DATETIME __DATE__ " " __TIME__
-#define PRODUCT_STRING "BoostXL G474 Motor"
-#define USBD_PID (0x100)
-#define MANUFACTURER_STRING "Unhuman Inc."
-
-#endif

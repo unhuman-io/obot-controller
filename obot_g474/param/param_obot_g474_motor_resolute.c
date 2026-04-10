efd1ae921f9464beeebb95f9b026da2d53f3aff6
Lee Magnusson <lee.magnusson@figure.ai>
2026-04-10 08:12:17 -0700

    make warnings into errors

--- obot_g474/param/param_obot_g474_motor_resolute.c
+++ obot_g474/param/param_obot_g474_motor_resolute.c
@@ -6,8 +6,8 @@
 const Param __attribute__ ((section ("flash_param"))) param_store = {
 #include "param_default.h"
 #include "param_motor.c"
-    .fast_loop_param.motor_encoder.cpr = 1 << 32,
-    .main_loop_param.output_encoder.cpr = 1 << 32,
+    .fast_loop_param.motor_encoder.cpr = UINT32_MAX,
+    .main_loop_param.output_encoder.cpr = (float) UINT32_MAX,
     .fast_loop_param.foc_param.pi_d.kp=5,
     .fast_loop_param.foc_param.pi_d.ki=.3,
     // .fast_loop_param.foc_param.pi_d.kp=5, // hd

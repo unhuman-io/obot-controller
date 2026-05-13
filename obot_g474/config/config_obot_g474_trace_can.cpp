61791a5785c8371ff734631adc316fd0e9c7c1bf
leemagnusson <leemagnusson@gmail.com>
2026-05-12 19:56:06 -0700

    Feature/coroutine (#350)
    * better circular limit
    
    * motorlib to develop
    
    * coroutine example
    
    * update coroutine
    
    * cycle stats delay until
    
    * use yield
    
    * coroutine for main maintenance
    
    * more async maintenance
    
    * initialize current timeout
    
    * motorlib to develop

--- obot_g474/config/config_obot_g474_trace_can.cpp
+++ obot_g474/config/config_obot_g474_trace_can.cpp
@@ -2,7 +2,8 @@
 #include "st_device.h"
 #include "../../motorlib/encoder.h"
 #include "../../motorlib/torque_sensor.h"
-
+#include <coroutine>
+#include "task.h"
 
 #define COMMS   COMMS_CAN
 
@@ -26,6 +27,31 @@ namespace config {
 
 #include "../../motorlib/boards/config_obot_g474_trace.cpp"
 
-void config_init() {}
+
+Task<> init_sensor(CycleScheduler& sched) {
+    co_await sched.async_delay_us(500);
+    logger.log("sensor mid");
+    co_await sched.async_delay_us(500);
+    logger.log("sensor done");
+}
+
+Task<> init_adc(CycleScheduler& sched) {
+    co_await sched.async_delay_us(1200); 
+    logger.log("adc done");
+}
+
+void config_init() {
+    CycleScheduler init_scheduler;
+
+    auto sensor_task = init_sensor(init_scheduler);
+    auto adc_task = init_adc(init_scheduler);
+
+    logger.log("init start");
+    while (!sensor_task.is_done() || !adc_task.is_done()) {
+        init_scheduler.poll();
+    }
+    logger.log("init done");
+}
 
 void config_maintenance() {}
+

715c50ba4daae52f315788b07c04d1ad59b1b341
Lee Magnusson <lee.magnusson@figure.ai>
2026-07-21 10:19:30 -0700

    possible structure

--- obot3/main.cpp
+++ obot3/main.cpp
@@ -1,7 +1,55 @@
 
+template <typename System, typename Config>
+struct MainLoop {
+    static void update() {
+        System::FastLoop::get_status();
+    }
+    static void get_command() {}
+};
 
+template <typename System, typename Config>
+struct FastLoop {
+    static void update() {
+        System::MainLoop::get_command();
+    }
+    static void get_status() {}
+};
+
+struct Config {
+    static constexpr int fast_loop_frequency = 1000;
+    static constexpr int main_loop_frequency = 100;
+    
+    struct MainLoopConfig {
+        static constexpr int frequency = main_loop_frequency;
+    };
+    
+    template <typename System>
+    using MainLoopType = MainLoop<System, MainLoopConfig>;
+
+    struct FastLoopConfig {
+        static constexpr int frequency = fast_loop_frequency;
+    };
+    
+    template <typename System>
+    using FastLoopType = FastLoop<System, FastLoopConfig>;
+
+};
+
+
+template<typename Cfg>
+struct System {
+    using MainLoop = typename Cfg::MainLoopType<System>;
+    using FastLoop = typename Cfg::FastLoopType<System>;
+};
+
+using MySystem = System<Config>;
 
 int main() {
+
+    while(1) {
+        MySystem::FastLoop::update();
+        MySystem::MainLoop::update();
+    }
     return 0;
 }
 
